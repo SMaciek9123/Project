9f2cdaa6d7c363d1f12c519e2f60295d8a9dc9a0
cokolwiek <cokolwiek.com>
2024-05-27 22:05:30 +0200

    Web Upgrade

--- server.py
+++ server.py
@@ -5,28 +5,86 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
 
+lobbies = {}
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/game-settings')
+def game_settings():
+    username = request.args.get('username')
+    room = request.args.get('room')
+    return render_template('game-settings.html', username=username, room=room)
+
+@app.route('/waiting-room')
+def waiting_room():
+    username = request.args.get('username')
+    room = request.args.get('room')
+    size = request.args.get('size')
+    return render_template('waiting-room.html', username=username, room=room, size=size)
+
+@app.route('/lobby-list')
+def lobby_list():
+    username = request.args.get('username')
+    return render_template('lobby-list.html', username=username)
+
+@app.route('/game')
+def game():
+    username = request.args.get('username')
+    room = request.args.get('room')
+    size = request.args.get('size')
+    return render_template('game.html', username=username, room=room, size=size)
+
+@socketio.on('create')
+def on_create(data):
+    username = data['username']
+    room = data['room']
+    lobbies[room] = {'host': username, 'size': None, 'players': [username]}
+    join_room(room)
+    emit('message', {'msg': f'{username} has created the room {room}.'}, room=room)
+
+@socketio.on('selectBoardSize')
+def on_select_board_size(data):
+    room = data['room']
+    size = data['size']
+    if room in lobbies:
+        lobbies[room]['size'] = size
+        emit('waitingForPlayer', room=room)
+
+@socketio.on('waitForPlayer')
+def on_wait_for_player(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    if len(lobbies[room]['players']) == 2:
+        emit('startGame', room=room)
+
+@socketio.on('getLobbies')
+def on_get_lobbies():
+    available_lobbies = [room for room in lobbies if len(lobbies[room]['players']) == 1 and lobbies[room]['size']]
+    emit('lobbies', available_lobbies)
 
 @socketio.on('join')
 def on_join(data):
     username = data['username']
     room = data['room']
-    join_room(room)
-    emit('message', {'msg': f'{username} has entered the room.'}, room=room)
+    if room in lobbies and len(lobbies[room]['players']) == 1:
+        lobbies[room]['players'].append(username)
+        join_room(room)
+        emit('message', {'msg': f'{username} has entered the room.'}, room=room)
+        emit('startGame', room=room)
 
-@socketio.on('leave')
-def on_leave(data):
-    username = data['username']
-    room = data['room']
-    leave_room(room)
-    emit('message', {'msg': f'{username} has left the room.'}, room=room)
-
-@socketio.on('send_message')
-def handle_send_message_event(data):
-    room = data['room']
-    emit('message', {'msg': data['msg'], 'username': data['username']}, room=room)
+@socketio.on('disconnect')
+def on_disconnect():
+    for room in lobbies:
+        if request.sid in lobbies[room]['players']:
+            lobbies[room]['players'].remove(request.sid)
+            if not lobbies[room]['players']:
+                del lobbies[room]
+            else:
+                emit('message', {'msg': 'A player has disconnected.'}, room=room)
+            break
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=8000, debug=True)
