0080cf88ba2be3453d7c06e7cd3cfcbfe7fb65db
cokolwiek <cokolwiek.com>
2024-05-27 13:55:40 +0200

    Roomy

--- server.py
+++ server.py
@@ -1,19 +1,32 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, join_room, leave_room, emit
 
 app = Flask(__name__)
-app.config['SECRETKEY'] = 'yoursecret_key'
+app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('message')
-def handle_message(msg):
-    print('Message: ' + msg)
-    emit('message', msg, broadcast=True)
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    emit('message', {'msg': f'{username} has entered the room.'}, room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    emit('message', {'msg': f'{username} has left the room.'}, room=room)
+
+@socketio.on('send_message')
+def handle_send_message_event(data):
+    room = data['room']
+    emit('message', {'msg': data['msg'], 'username': data['username']}, room=room)
 
 if __name__ == '__main__':
-    #socketio.run(app)
-    app.run(host="0.0.0.0", port=8000)
+    socketio.run(app, host='0.0.0.0', port=8000, debug=True)
