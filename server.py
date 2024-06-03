c21bc4f212e63f152c61c806c692076858cf1ed2
konrad-bol <kbolesta02@gmail.com>
2024-06-03 10:53:51 +0200

    front-end wal
    cos

--- server.py
+++ server.py
@@ -5,11 +5,22 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
 
+users = []
 lobbies = {}
+game_data = {}# {'room' => {'user1'=> (gotowosc,tura),
+              #             'user2' =>(gotowosc,tura)}} 
+
+boards = {} # {'room' => {'user1'=> 'board',
+            #             'user2' =>'board2'}} 
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/functions.js')
+def functions():
+    return render_template('functions.js')
 
 @app.route('/game-settings')
 def game_settings():
@@ -26,6 +37,7 @@ def waiting_room():
 
 @app.route('/lobby-list')
 def lobby_list():
+    print(users)
     username = request.args.get('username')
     return render_template('lobby-list.html', username=username)
 
@@ -36,21 +48,61 @@ def game():
     size = request.args.get('size')
     return render_template('game.html', username=username, room=room, size=size)
 
+
+@socketio.on('create_user')
+def create_user(data):
+    username = data['username']
+    users.append(username)
+    print(users)
+
+
+@socketio.on('checkUsername')
+def give_users(username):
+    print("username "+username);
+    if username in users:
+        emit('checkUsername', True)
+    else:
+        emit('checkUsername', False)
+
+
 @socketio.on('create')
 def on_create(data):
     username = data['username']
     room = data['room']
     lobbies[room] = {'host': username, 'size': None, 'players': [username]}
     join_room(room)
-    emit('message', {'msg': f'{username} has created the room {room}.'}, room=room)
 
 @socketio.on('selectBoardSize')
 def on_select_board_size(data):
     room = data['room']
     size = data['size']
     if room in lobbies:
-        lobbies[room]['size'] = size
+        board = create_board(size)
+        host = lobbies[room]['host']
+        lobbies[room]['size'] = size #nwm po co to
+        game_data[room]= {host: (False,False)}
+        boards[room]={host: board}
+        print(boards[room][host])
         emit('waitingForPlayer', room=room)
+
+
+def create_board(size):
+    return [['O' for _ in range(int(size))] for _ in range(int(size))]
+
+
+@socketio.on('giveBoard')
+def on_give_board(room):
+    host= lobbies[room]['host']
+    print(boards[room][host])
+    emit('giveBoard', boards[room][host])
+
+@socketio.on('giveData')
+def on_give_data(data):
+    username = data['username']
+    room = data['room']
+    print(game_data[room][username])
+    emit('giveData',  game_data[room][username])
+
 
 @socketio.on('waitForPlayer')
 def on_wait_for_player(data):
@@ -60,6 +112,7 @@ def on_wait_for_player(data):
     if len(lobbies[room]['players']) == 2:
         size = lobbies[room]['size']
         emit('startGame', {'size': size}, room=room)
+
 
 @socketio.on('getLobbies')
 def on_get_lobbies():
@@ -72,9 +125,14 @@ def on_join(data):
     room = data['room']
     if room in lobbies and len(lobbies[room]['players']) == 1:
         lobbies[room]['players'].append(username)
+        host = lobbies[room]['host']
         join_room(room)
         size = lobbies[room]['size']
-        emit('message', {'msg': f'{username} has entered the room.'}, room=room)
+        boards[room][username]= boards[room][host]
+        game_data[room][username]= (False,False)
+        print("\n")
+        print(boards[room])
+        print("\n")
         emit('startGame', {'size': size}, room=room)
 
 @socketio.on('disconnect')
@@ -88,5 +146,20 @@ def on_disconnect():
                 emit('message', {'msg': 'A player has disconnected.'}, room=room)
             break
 
+
+
+
+@socketio.on('shoot')
+def on_shoot(data):
+    room = data['room']
+
+    x = (data['x'])
+    y = (data['y'])
+    print(x)
+    print(y)
+    boards[room][x][y]='1'
+    print(boards[room])
+
+
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8002, debug=True)
---
+++ templates/functions.js
@@ -0,0 +1,90 @@
+async function isLogged(){
+  if(document.cookie == "")
+  {
+      return false;
+  }
+  var result = await compareCookie();
+  if(result == true)
+  {
+      alert("zalogowano jako "+getCookie('username'));
+      return true;
+  }
+  else
+  {
+      alert("zaloguj się");
+      return false;
+  }
+}
+
+function setCookie(cname, cvalue, exdays) {
+const d = new Date();
+d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+let expires = "expires="+d.toUTCString();
+document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+let name = cname + "=";
+let ca = document.cookie.split(';');
+for(let i = 0; i < ca.length; i++) {
+  let c = ca[i];
+  while (c.charAt(0) == ' ') {
+    c = c.substring(1);
+  }
+  if (c.indexOf(name) == 0) {
+    return c.substring(name.length, c.length);
+  }
+}
+return "";
+}
+
+async function compareCookie() {
+  var socket = io();
+  username=getCookie('username');
+  //alert(username);
+  //socket.emit('getUsers');
+  var result = await asyncEmit('checkUsername', username);
+  //alert(result);
+  return result;
+}
+
+async function asyncEmit(eventName, data) {
+var socket = io();
+return new Promise(function (resolve, reject) {
+//alert("dzialam");
+//alert(eventName);
+  socket.emit(eventName, data);
+  socket.on(eventName, result => {
+    socket.off(eventName);
+    resolve(result);
+  });
+  setTimeout(reject, 1000);
+});
+}
+
+async function getBoard(room){
+  var board = await asyncEmit('giveBoard', room); //dodać który board ma zwrócic (room, player)
+  alert("getBoard alert: "+board);
+  return board;
+}
+
+async function getData(room,username){
+  var data = await asyncEmit('giveData', {'room': room,'username': username}); //dodać który board ma zwrócic (room, player)
+  alert("getData alert: "+data);
+  return data;
+}
+
+async function shoot_game(){
+  var data = await asyncEmit('getData',room); //update data
+  if(game_not_over) // jezeli gra dalej jest
+    {
+        if(player_turn=id_player)// jezeli gracz ma swoja ture
+          {
+            var new_data = await asyncEmit('shoot',room);
+          }
+          else//jak nie to poczekaj na nia
+          {
+            wait_for_turn();
+          }
+    }
+}
